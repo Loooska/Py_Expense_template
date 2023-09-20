94b73c9e9b348a32dd6fe8c44422caea65a1de7c
Looska <yann.renaud@epita.fr>
2023-09-20 21:52:10 +0200

    final send

--- expense.py
+++ expense.py
@@ -1,4 +1,7 @@
+import csv
 from PyInquirer import prompt
+
+path_file_expense = 'expense_report.csv'
 
 expense_questions = [
     {
@@ -19,10 +22,20 @@ expense_questions = [
 
 ]
 
-
+# Fonction pour écrire les données dans un fichier CSV
+#def write_expense(data):
+ #   with open("./expense_report.csv", mode='a', newline='') as fichier:
+  #      writer = csv.writer(fichier)
+   #     writer.writerow(data.values())
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+    print(infos)
+    file = open(path_file_expense, mode='a')
+    writer = csv.writer(file)
+    writer.writerow(['amont: ' + infos['amount'], 'label: ' + infos['label'], 'spender: ' + infos['spender']])
+    file.close()
+    #write_expense(data)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
--- user.py
+++ user.py
@@ -1,8 +1,24 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+path_file_user = 'users.csv'
+
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New user - Name: "
+    }
 ]
 
 def add_user():
+    infos = prompt(user_questions)
+    print(infos)
+    file = open(path_file_user, mode='a')
+    writer = csv.writer(file)
+    writer.writerow([infos['name']])
+    file.close()
+    print("New user added !")
+
     # This function should create a new user, asking for its name
     return
